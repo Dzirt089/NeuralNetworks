705eae9d52c55d625bda38d05e38de739da7a193
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2025-01-03 19:50:48 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,19 @@
+﻿namespace NeuralNetworks
+{
+	public class Neuron
+	{
+		/// <summary>
+		/// Вес нейрона
+		/// </summary>
+		public List<double> Weight { get; }
+		/// <summary>
+		/// Тип нейрона
+		/// </summary>
+		public NeuronType Type { get; }
+
+		/// <summary>
+		/// Сохраняем состояние
+		/// </summary>
+		public double Output { get; private set; }
+	}
+}
